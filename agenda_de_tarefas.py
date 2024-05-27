5344707d3a0c34e6430673780447295e4eb3e813
Murilo Oliveira <84341631+murilooliveira18@users.noreply.github.com>
2024-05-27 18:35:24 -0300

    Importando código de agenda de tarefas

---
+++ agenda_de_tarefas.py
@@ -0,0 +1,55 @@
+from tkinter import *
+import tkinter.messagebox
+
+def entertask():
+    input_text = ""
+    def add():
+        input_text=entry_task.get(1.0, "end-1c")
+        if input_text=="":
+            tkinter.messagebox.showwarning(title="Cuidado!", message="Por favor insira algum texto")
+        else:
+            listbox_task.insert(END, input_text)
+            root1.destroy()
+    root1=Tk()
+    root1.title("Adicione uma tarefa")
+    entry_task=Text(root1, width=40, height=4)
+    entry_task.pack()
+    button_temp=Button(root1,text="Adicione uma tarefa",command=add)
+    button_temp.pack()
+    root1.mainloop()
+
+def deletetask():
+    selected=listbox_task.curselection()
+    listbox_task.delete(selected[0])
+
+def markcompleted():
+    marked=listbox_task.curselection()
+    temp=marked[0]
+    temp_marked=listbox_task.get(marked)
+    temp_marked=temp_marked+" ✅"
+    listbox_task.delete(temp)
+    listbox_task.insert(temp,temp_marked)
+window=Tk()
+window.title("Agenda de tarefas em python")
+
+frame_task=Frame(window)
+frame_task.pack()
+
+listbox_task=Listbox(frame_task,bg="black",fg="white",height=15,width=50,font="Helvetica")
+listbox_task.pack(side=tkinter.LEFT)
+
+scrollbar_task=Scrollbar(frame_task)
+scrollbar_task.pack(side=tkinter.RIGHT,fill=tkinter.Y)
+listbox_task.config(yscrollcommand=scrollbar_task.set)
+scrollbar_task.config(command=listbox_task.yview)
+
+entry_button=Button(window,text="Adicione uma tarefa",width=50,command=entertask)
+entry_button.pack(pady=3)
+
+delete_button=Button(window,text="Remova uma tarefa",width=50,command=deletetask)
+delete_button.pack(pady=3)
+
+mark_button=Button(window,text="Marque como completa",width=50,command=markcompleted)
+mark_button.pack(pady=3)
+
+window.mainloop()
